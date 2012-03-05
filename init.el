0599d9659c39516933e7097b0bbd5606c445b546
Piyush Nimbalkar <piyushmn@thoughtworks.com>
2012-03-05 21:01:09 +0530

    js2-mode for javascript with some small customizations. Have to fix few of its indentation issues

--- init.el
+++ init.el
@@ -238,3 +238,22 @@
 
 (global-set-key [(C =)] 'text-scale-increase)
 (global-set-key [(C -)] 'text-scale-decrease)
+
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/js2-mode"))
+(require 'js2-mode)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(js2-auto-indent-p t)
+ '(js2-basic-offset 2)
+ '(js2-cleanup-whitespace t)
+ '(js2-indent-on-enter-key t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
