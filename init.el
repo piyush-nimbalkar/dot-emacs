84e14666181fe81ca56d05e1d377eb7a32b45452
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2012-10-22 02:25:27 +0530

    Initailizing js2-mode on .js files

--- init.el
+++ init.el
@@ -262,12 +262,10 @@
  '(js2-basic-offset 2)
  '(js2-cleanup-whitespace t)
  '(js2-indent-on-enter-key t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+
+;; Js2 Mode
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;; Yaml Mode
 (add-to-list 'load-path "~/.emacs.d/elpa/yaml-mode-0.0.5")
