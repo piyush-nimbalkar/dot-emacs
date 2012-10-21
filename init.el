1bd72b37a64ceed99666cf541609280ca83c4a02
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2012-10-22 02:27:37 +0530

    Forcing myself again to avoid use of arrow keys

--- init.el
+++ init.el
@@ -289,3 +289,8 @@
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
 (require 'zencoding-trie)
+
+;; No Easy Keys
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'no-easy-keys)
+(no-easy-keys 1)
