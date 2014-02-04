6967e92142c7358c94f4d856d7cdf25de344ce15
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-02-04 17:34:17 -0500

    Erlang mode. This is not portable. Will fix it sometime.

--- init.el
+++ init.el
@@ -361,3 +361,17 @@
 
 
 (global-set-key (kbd "C-c t") 'org-set-tags)
+
+
+;; Erlang Mode
+(setq load-path (cons  "/opt/otp_src_R16B/lib/tools/emacs"
+                       load-path))
+(setq erlang-root-dir "/opt/otp_src_R16B")
+(setq exec-path (cons "/opt/otp_src_R16B/bin" exec-path))
+(require 'erlang-start)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
