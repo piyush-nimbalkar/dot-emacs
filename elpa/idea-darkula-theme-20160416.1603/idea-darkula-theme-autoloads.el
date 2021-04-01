3e0f0833b324d4b16e8ca7f9dfeed45abd18bd87
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2021-03-31 17:20:00 -0700

    Add idea darkula theme
    Signed-off-by: Piyush Nimbalkar <piyushmnimbalkar@gmail.com>

---
+++ elpa/idea-darkula-theme-20160416.1603/idea-darkula-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; idea-darkula-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "idea-darkula-theme" "idea-darkula-theme.el"
+;;;;;;  (23024 31580 981713 554000))
+;;; Generated autoloads from idea-darkula-theme.el
+
+(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("idea-darkula-theme-pkg.el") (23024 31580
+;;;;;;  992271 594000))
+
+;;;***
+
+(provide 'idea-darkula-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; idea-darkula-theme-autoloads.el ends here
---
+++ elpa/idea-darkula-theme-20160416.1603/idea-darkula-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "idea-darkula-theme" "20160416.1603" "Color theme based on IntelliJ IDEA Darkula color theme" (quote ((emacs "24.1"))))
